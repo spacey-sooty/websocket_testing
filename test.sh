1b57aabf3fa3c54cdf04d118fcf736865888e5db
Isaac Turner <spacey_sooty@outlook.com>
2023-10-24 19:45:13 +0800

    use websocat

--- test.sh
+++ test.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/sh
-curl -i -N  \
-    -H "Connection: Upgrade"  \
-    -H "Upgrade: websocket"  \
-    -H "Host: 127.0.0.1:8080"  \
-    -H "Origin: 127.0.0.1:8080"  \
-   127.0.0.1:8080
+websocat -s 8080
 
